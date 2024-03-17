a2509fdbe71b9db62c697b91d0abbc5c5266142d
Matheus Casante <matheus.casante@gmail.com>
2024-03-17 19:40:46 -0300

    refactor: clean up overview component

--- components/WOverview.vue
+++ components/WOverview.vue
@@ -6,12 +6,12 @@ const locationStore = useLocationStore();
 const newLocation = ref("");
 
 const handleCreate = () => {
+  if (!newLocation.value) return;
   locationStore.addLocation(newLocation.value, "name");
   newLocation.value = "";
 };
 
 const handleDelete = (location: ILocation) => {
-  console.log(location);
   locationStore.deleteLocation(location.slug);
 };
 </script>
@@ -19,48 +19,50 @@ const handleDelete = (location: ILocation) => {
 <template>
   <div class="overview">
     <div
-      v-for="(location, index) in locationStore.list"
-      class="rounded-2xl flex items-center justify-between overflow-hidden border-2 border-w-gray bg-w-rainbow"
+      v-for="location in locationStore.list"
+      class="card bg-w-rainbow"
       :key="location.slug"
     >
-      <NuxtLink
-        class="transition-all rounded-lg px-4 py-2 m-4 hover:px-8 hover:py-6 hover:m-0 font-semibold hover:grow hover:rounded-r-none bg-white"
-        :to="location.slug"
-      >
+      <NuxtLink class="location" :to="location.slug">
         {{ location.name }}
       </NuxtLink>
 
-      <button
-        class="transition-all rounded-lg px-4 py-2 flex items-center gap-2 bg-white px-8 py-6 rounded-l-none hover:bg-w-black border-l-2 border-w-gray"
-        @click="() => handleDelete(location)"
-      >
-        ❌
-      </button>
+      <button @click="() => handleDelete(location)">❌</button>
     </div>
 
-    <div
-      class="rounded-2xl flex items-center justify-between overflow-hidden border-2 border-w-gray bg-w-rainbow"
-    >
+    <div class="card bg-w-rainbow">
       <input
-        class="transition-all rounded-lg px-4 py-2 m-4 hover:px-8 hover:py-6 hover:m-0 font-semibold hover:grow hover:rounded-r-none bg-white outline-none w-full"
+        class="location"
         placeholder="Add new location"
         v-model="newLocation"
+        @keypress.enter="handleCreate"
       />
 
-      <button
-        class="transition-all rounded-lg px-4 py-2flex items-center gap-2 bg-white px-8 py-6 rounded-l-none hover:bg-w-green border-l-2 border-w-gray w-max"
-        @click="handleCreate"
-      >
-        🗺️
-      </button>
+      <button @click="handleCreate">🗺️</button>
     </div>
   </div>
 </template>
 
-<style>
+<style scoped lang="scss">
 .overview {
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
   @apply gap-4;
+}
+
+.card {
+  @apply rounded-2xl flex items-center justify-between overflow-hidden border-2 border-w-gray;
+
+  .location {
+    @apply transition-all rounded-lg px-4 py-2 m-4 hover:px-8 hover:py-6 hover:m-0 font-semibold hover:grow hover:rounded-r-none bg-white;
+
+    &:is(input) {
+      @apply outline-none w-full;
+    }
+  }
+
+  button {
+    @apply transition-all rounded-lg px-4 py-2 flex items-center gap-2 bg-white px-8 py-6 rounded-l-none hover:bg-w-black border-l-2 border-w-gray duration-300 ease-in-out;
+  }
 }
 </style>
