39aea7cfbabe41935bfcdebd49d340f1679dad20
Matheus Casante <matheus.casante@gmail.com>
2024-03-17 17:25:53 -0300

    refactor: improve swipe handling

--- composables/useCarousel.ts
+++ composables/useCarousel.ts
@@ -1,5 +1,6 @@
 import type { Ref } from "vue";
 import { useClamp } from "@vueuse/math";
+import type { UseSwipeReturn, UsePointerSwipeReturn } from "@vueuse/core";
 
 interface Options {
   maxWidth?: number;
@@ -31,23 +32,43 @@ export function useCarousel(
 
   const containerWidth = computed(() => length.value * itemWidth.value);
 
-  const { isSwiping, distanceX: lengthX } = usePointerSwipe(target, {
-    threshold: 10,
-    onSwipeEnd: () => {
-      const tolerance = Math.sign(lengthX.value) * 0.4;
+  const handleSwipe = (swipe: UseSwipeReturn | UsePointerSwipeReturn) => {
+    const distance =
+      (swipe as UseSwipeReturn)?.lengthX?.value ||
+      (swipe as UsePointerSwipeReturn).distanceX.value;
 
-      const draggedSlides = Math.round(
-        lengthX.value / itemWidth.value + tolerance
-      );
+    const tolerance = Math.sign(distance) * 0.4;
 
-      currentIndex.value += draggedSlides;
-    },
+    const draggedSlides = Math.round(distance / itemWidth.value + tolerance);
+
+    currentIndex.value += draggedSlides;
+  };
+
+  const isTouchDevice = useMediaQuery("(pointer: coarse)");
+
+  const useSmartSwipe = computed(() =>
+    isTouchDevice.value ? useSwipe : usePointerSwipe
+  );
+
+  const swipe = computed<UseSwipeReturn | UsePointerSwipeReturn>(() => {
+    const s: UseSwipeReturn | UsePointerSwipeReturn = useSmartSwipe.value(
+      target,
+      {
+        threshold: 10,
+        onSwipeEnd: () => handleSwipe(s),
+        pointerTypes: ["mouse"],
+      }
+    );
+
+    return s;
   });
 
   return {
     currentIndex,
-    isSwiping,
-    lengthX,
+    isSwiping: swipe.value.isSwiping,
+    lengthX:
+      (swipe.value as UseSwipeReturn).lengthX ??
+      (swipe.value as UsePointerSwipeReturn).distanceX,
     boundings,
     itemWidth,
     containerWidth,
